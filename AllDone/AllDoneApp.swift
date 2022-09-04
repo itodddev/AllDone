2dc7a708e0dbbad5d83f7707c7fe3c337b05c0b3
Todd James <todd@itodd.co>
2022-09-04 09:42:59 -0400

    Initial Commit

---
+++ AllDone/AllDoneApp.swift
@@ -0,0 +1,17 @@
+//
+//  AllDoneApp.swift
+//  AllDone
+//
+//  Created by Todd James on 9/4/22.
+//
+
+import SwiftUI
+
+@main
+struct AllDoneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
